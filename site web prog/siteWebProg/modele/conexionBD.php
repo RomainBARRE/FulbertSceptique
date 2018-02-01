a6bbc33400d35ee81feb5f10e9d7070bfb709677
RomainBARRE <Romain.barre28360@laposte.net>
2018-02-01 11:46:39 +0100

    dsnvhjdeggfyrs

--- site web prog/siteWebProg/modele/conexionBD.php
+++ site web prog/siteWebProg/modele/conexionBD.php
@@ -2,7 +2,7 @@
 	try {
 	$base = new PDO('mysql:host=localhost; dbname=G1LEAD', 'G1S1', 'bts1');
 	$base->exec('SET NAMES utf8');
-	$resultat = $base->query('SELECT * FROM organisation ORDER BY id=1');
+	$resultat = $base->query('SELECT * FROM organisation WHERE id=1');
 	}
 	catch (exception $e) {
 	    die('Erreur '.$e->getMessage());

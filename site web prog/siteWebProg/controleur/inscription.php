7addb1c7bb5b26462658e4565bc8349a8172bc8b
RomainBARRE <Romain.barre28360@laposte.net>
2018-02-01 11:23:18 +0100

    on reprend

--- site web prog/siteWebProg/controleur/inscription.php
+++ site web prog/siteWebProg/controleur/inscription.php
@@ -1,17 +1,23 @@
-<!DOCTYPE HTML>
-
-<html>
-	<body>
+<!DOCTYPE html>
+	<html>
 		<head>
 			<title>Accueil FulbertSceptique</title>
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="../contenu/css.css" />
 			<link rel="icon" href="../images/fraise.png"/>
 		</head>
-	</body>
+		<body>
+			<header>
+				<?php 
+					include('vue/menu.html');
+				?>
+			</header>
+				<?php 
+				?>
+			<footer>
+				<?php
+					include('../vue/basPage.html');
+				?>
+			</footer>
+		</body>
 </html>
-
-<?php 
-include('../vue/menu.html');
-include('../vue/basPage.html');
-?>
--- site web prog/siteWebProg/index.php
+++ site web prog/siteWebProg/index.php
@@ -1,19 +1,25 @@
-<!DOCTYPE HTML>
-
-<html>
-	<body>
+<!DOCTYPE html>
+	<html>
 		<head>
 			<title>Accueil FulbertSceptique</title>
 			<meta charset="utf-8" />
 			<link rel="stylesheet" href="contenu/css.css" />
 			<link rel="icon" href="images/fraise.png"/>
 		</head>
-	</body>
+		<body>
+			<header>
+				<?php 
+					include('vue/menuInd.html');
+					include('vue/banniere.html');
+				?>
+			</header>
+				<?php 
+					include('vue/corps.html');
+				?>
+			<footer>
+				<?php
+					include('vue/basPageInd.html');
+				?>
+			</footer>
+		</body>
 </html>
-
-<?php 
-include('vue/menuInd.html');
-include('vue/banniere.html');
-include('vue/corps.html');
-include('vue/basPage.html');
-?>
